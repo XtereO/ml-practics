d0d494a79195730ade576aafe9c47029babe1af3
XtereO <kolahola149@gmail.com>
2025-10-08 15:56:44 +0300

    decision_tree with imbalanced sample

---
+++ decision_tree/loan_classifier.py
@@ -0,0 +1,67 @@
+import sys
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import zscore
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from imblearn.over_sampling import SMOTE
+
+
+def prepare_data(X_train, X_test, y_train, y_test):
+    def get_fit_model(index, criterion, class_weight=None):
+        model = DecisionTreeClassifier(
+            random_state=121, criterion=criterion, class_weight=class_weight)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        print(f"f1 score of model {index}", f1_score(y_test, y_pred))
+        return model
+    return get_fit_model
+
+
+data = pd.read_csv("Bank_Personal_Loan_Modelling_train.csv")
+data = data.drop("ID", axis=1)
+print("data head", data.describe())
+
+if len(sys.argv) > 1 and sys.argv[1] == "1":
+    sns.heatmap(data=data.corr(), annot=True)
+    plt.show()
+
+data = data.drop("ZIP Code", axis=1)
+
+label = "Personal Loan"
+data["Experience"] -= data["Experience"].min()
+print("mean shifted exp", data["Experience"].mean())
+
+data["CCAvg"] *= 12
+print("mean multiplied CCAvg", data["CCAvg"].mean())
+
+cols = data.columns
+if len(sys.argv) > 1 and sys.argv[1] == "2":
+    for col in cols:
+        plt.title(col)
+        sns.boxplot(data[col])
+        plt.show()
+
+if len(sys.argv) > 1 and sys.argv[1] == "3":
+    sns.histplot(data=data["Mortgage"])
+    plt.show()
+
+z_mortgage = zscore(data["Mortgage"])
+print("Mortgage |zscore|>3", z_mortgage[abs(z_mortgage) > 3].size)
+
+data = data[abs(z_mortgage) <= 3]
+print("Remain data size", data.shape)
+
+X = data.drop("Personal Loan", axis=1)
+y = data["Personal Loan"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=121, stratify=data["Personal Loan"])
+
+get_fit_model1 = prepare_data(X_train, X_test, y_train, y_test)
+model1 = get_fit_model1(1, "gini")
+model2 = get_fit_model1(2, "entropy")
+model3 = get_fit_model1(3, "gini", "balanced")
+model4 = get_fit_model1(4, "entropy", "balanced")
